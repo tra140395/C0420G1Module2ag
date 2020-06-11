410447cd99c1c2ee95d48bcc1c30f6b5760665e1
tra140395 <sontra140395@gmail.com>
2020-06-11 09:00:40 +0700

    nop bt

---
+++ src/b4oop/baitap/QuadraticEquation.java
@@ -0,0 +1,35 @@
+package b4oop.baitap;
+
+import java.util.Scanner;
+
+public class QuadraticEquation {
+    private double a;
+    private double b;
+    private double c;
+    private double delta;
+    private double root1;
+    private double root2;
+    public double  getDiscriminant() {
+        return delta =(this.b*this.b)-(4*this.a*this.c);
+    }
+    public QuadraticEquation(double a,double b,double c) {
+       this.a = a;
+       this.b = b;
+       this.c = c;
+   }
+    public double getA(){
+       return this.a;
+   }
+    public double getB(){
+       return this.b;
+   }
+    public double getC(){
+       return this.c;
+   }
+   public double getRoot1(){
+        return root1 = (((-b)-(Math.pow(delta,0.5)))/(2*a));
+   }
+   public double getRoot2(){
+        return root2 = (((-b)+(Math.pow(delta,0.5)))/(2*a));
+   }
+}
